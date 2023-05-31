13247cff8f56c767a50b49900463c3685a8b783a
James Barber <jamesbarber@twinsbaseball.com>
2023-05-31 08:21:26 -0400

    Access to the Template with @ViewChild

--- data-binding/src/app/cockpit/cockpit.component.ts
+++ data-binding/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -16,7 +23,9 @@ export class CockpitComponent implements OnInit {
   }>();
   // serverElements = [];
   // newServerName = '';
-  newServerContent = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput', { static: true })
+  serverContentInput: ElementRef;
   constructor() {}
 
   ngOnInit(): void {}
@@ -24,14 +33,14 @@ export class CockpitComponent implements OnInit {
   onAddServer(nameInput: HTMLInputElement) {
     this.serverCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent,
+      serverContent: this.serverContentInput.nativeElement.value,
     });
   }
 
   onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent,
+      serverContent: this.serverContentInput.nativeElement.value,
     });
   }
 }
