e0afe7a1d3d5f9fe2da9ed53af30ab6136a77994
James Barber <jamesbarber@twinsbaseball.com>
2023-08-11 09:19:23 -0400

    Build custom observable; Work with errors & completion

--- observables/src/app/home/home.component.ts
+++ observables/src/app/home/home.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  private firstObsSubscription: Subscription;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    // this.firstObsSubscription = interval(1000).subscribe((count) => {
+    //   console.log(count);
+    // });
+    const customIntervalObservable = Observable.create((observer) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if (count == 2) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error('Count is greater than 3!'));
+        }
+        count++;
+      }, 1000);
+    });
+
+    this.firstObsSubscription = customIntervalObservable.subscribe(
+      (data) => {
+        console.log(data);
+      },
+      (error) => {
+        console.log(error);
+        alert(error.message);
+      },
+      () => {
+        console.log('Completed!');
+      }
+    );
   }
 
+  ngOnDestroy(): void {
+    this.firstObsSubscription.unsubscribe();
+  }
 }
