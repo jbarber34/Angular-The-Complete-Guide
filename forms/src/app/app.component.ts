b98c2c6fd3cfe1223a96e16ab43a284b0857c4aa
James Barber <jamesbarber@twinsbaseball.com>
2023-08-21 08:25:51 -0400

    Set default form values with ngModel property binding; Use ngModel w/two-way binding

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -8,6 +8,8 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
+  defaultQuestion = 'pet';
+  answer: string;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
