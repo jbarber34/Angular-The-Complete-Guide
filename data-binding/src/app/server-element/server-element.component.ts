e7ce817ad7d6aedca4cad543331cabf456a9e075
James Barber <jpbarber34@gmail.com>
2023-10-12 13:22:48 -0400

    Understanding View Encapsulation

--- data-binding/src/app/server-element/server-element.component.ts
+++ data-binding/src/app/server-element/server-element.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated,
 })
 export class ServerElementComponent implements OnInit {
   // By setting alia 'srvElement', now have to use 'srvElement' for binding in HTML, element will no longer work
