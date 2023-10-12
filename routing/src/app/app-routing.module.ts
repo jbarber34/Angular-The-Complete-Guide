934f6aeaeb7a031432e8c2881ce89a9376c10e46
James Barber <jpbarber34@gmail.com>
2023-10-12 13:22:49 -0400

    Rename app routing

--- routing/src/app/app-routing.module.ts
+++ routing/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { ServersComponent } from './servers/servers.component';
 import { UserComponent } from './users/user/user.component';
 import { UsersComponent } from './users/users.component';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './auth-guard.service';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -17,6 +18,7 @@ const appRoutes: Routes = [
   },
   {
     path: 'servers',
+    canActivate: [AuthGuard],
     component: ServersComponent,
     children: [
       { path: ':id', component: ServerComponent },
