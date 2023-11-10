087c718383ad35dac7ba3e1cfd43084e26262118
James Barber <jpbarber34@gmail.com>
2023-11-10 09:33:38 -0500

    Create response interceptors and use multiple interceptors

--- http/src/app/auth-interceptor.service.ts
+++ http/src/app/auth-interceptor.service.ts
@@ -6,7 +6,6 @@ import {
 
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
-    console.log('Request is on its way');
     // return next.handle(req);
     const modifiedRequest = req.clone({
       headers: req.headers.append('Auth', 'xyz'),
