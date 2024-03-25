65a648f97b0c2db908c3908d9a8f37e605cb8531
James Barber <jpbarber34@gmail.com>
2024-03-25 08:21:11 -0400

    More standalone directive building blocks; Migrating another component

--- standalone-components/src/app/app.module.ts
+++ standalone-components/src/app/app.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
-import { WelcomeComponent } from './welcome/welcome.component';
 import { DetailsComponent } from './welcome/details/details.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, WelcomeComponent],
-  imports: [BrowserModule, DetailsComponent],
+  declarations: [AppComponent],
+  imports: [BrowserModule, DetailsComponent, WelcomeComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
