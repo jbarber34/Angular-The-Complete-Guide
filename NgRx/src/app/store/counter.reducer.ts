807f4e585e7187e56386702856b6f061ff420ca4
James Barber <jpbarber34@gmail.com>
2024-05-29 07:03:17 -0700

    Attach data to actions

--- NgRx/src/app/store/counter.reducer.ts
+++ NgRx/src/app/store/counter.reducer.ts
@@ -4,6 +4,6 @@ import { decrement, increment } from './counter.actions';
 const initialState = 0;
 export const counterReducer = createReducer(
   initialState,
-  on(increment, (state) => state + 1),
-  on(decrement, (state) => state - 1)
+  on(increment, (state, action) => state + action.value),
+  on(decrement, (state, action) => state - action.value)
 );
